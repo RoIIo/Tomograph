e590ced613da1298d164002479b70e6d7c22692e
RoIIo <73474236+RoIIo@users.noreply.github.com>
2021-04-01 11:49:48 +0200

    added filters

--- main.py
+++ main.py
@@ -16,19 +16,28 @@ import matplotlib.pyplot as plt
 
 from skimage.transform import radon, iradon
 
+from pydicom.dataset import Dataset, FileDataset
+from pydicom.data import get_testdata_files
+import pydicom
+import datetime
+
 detectors = 180  # number of detectors
 deltaAlfa = 1  # degrees between (n) and (n+1) emitter
 width = 180  # degrees between first and the last detector
 
 
 def load_image(filename):  # Load image
+    if filename.endswith(".dcm"):
+        image=pydicom.dcmread(filename).pixel_array
+        startimage=IMG.fromarray(np.uint8(image))
+        startimage.save('output.png')
+    else:
+        im = IMG.open(filename)
+        sqrWidth = np.ceil(np.sqrt(im.size[0] * im.size[1])).astype(int)
+        im_resize = im.resize((sqrWidth, sqrWidth))
+        im_resize.save('output.png')
 
-    im = IMG.open(filename)
-    sqrWidth = np.ceil(np.sqrt(im.size[0] * im.size[1])).astype(int)
-    im_resize = im.resize((sqrWidth, sqrWidth))
-    im_resize.save('output.png')
-
-    image = cv2.imread('output.png', 0)
+        image = cv2.imread('output.png', 0)
 
     return image
 
@@ -67,17 +76,40 @@ def radon_t(image):  # Radon Transformation
 
             for idx, k in enumerate(index):
                 sinogram[i][j] += image[index[idx][0]+radius-1][index[idx][1]+radius-1]
+        if (anime.get() == 1):
+            smaximum = sinogram.max()
+            ss=np.divide(sinogram,smaximum)
+            ss=np.multiply(ss,255)
+            sinogram_to_array = IMG.fromarray(np.array(ss))
+            sinogram_photo = IMGtk.PhotoImage(sinogram_to_array)
+            sinogram1.create_image(0, 0, image=sinogram_photo, anchor='nw')
+            root.update()
+
+    #sinogram = filter_sinogram(sinogram)
 
     maximum = sinogram.max()
     sinogram = np.divide(sinogram,maximum)
     sinogram = np.multiply(sinogram,255)
+    if(filtr.get()==1):
+        sinogram = filter_sinogram(sinogram)
+        sinogram=sinogram*8
     return sinogram
 
 
-def i_radon_t(sinogram,side):  # Inverse Radon Transformation
+def filter_sinogram(sinogram):
+
+    for i in range(len(sinogram)):
+        sinogram[i]=np.real(scipy.fft.ifft(scipy.fft.fft(sinogram[i])*np.hamming(len(sinogram[i]))))
+
+    return sinogram
+
+def i_radon_t(sinogram,side,inp):  # Inverse Radon Transformation
     steps = floor(360 / deltaAlfa)
     radius = floor(side / 2)
     image = np.zeros((side,side), dtype=float)
+
+    #sinogram = filter_sinogram(sinogram)
+
     for i in range(steps):
         print(i)
         # calculate positions of emitter and detectors
@@ -89,10 +121,19 @@ def i_radon_t(sinogram,side):  # Inverse Radon Transformation
 
             for idx, k in enumerate(index):
                 image[index[idx][0]+radius-1][index[idx][1]+radius-1] += sinogram[i][j]
+        if(anime.get()==1):
+            ii = np.divide(image,255)
+            output_to_array = IMG.fromarray(np.array(ii))
+            output_photo = IMGtk.PhotoImage(output_to_array)
+            output.create_image(0, 0, image=output_photo, anchor='nw')
+            root.update()
+        out=np.divide(image,255)
+        MSE_val.set("RMSE:" + str(process_MSE(inp, out)))
     image = np.divide(image,255)
+
     return image
 
-def animation(sinogram):
+def animations(sinogram):
     for i in range(360//deltaAlfa):
         sinogram_to_array = IMG.fromarray(np.array(sinogram[:i]))
         sinogram_photo = IMGtk.PhotoImage(sinogram_to_array)
@@ -127,7 +168,7 @@ def Computing():
     set_options()
 
     #Get and load image
-    filename = directory + pic_input.get()
+    filename = pic_input.get()
     img = load_image(filename)
 
     # SHOW INPUT IMAGE
@@ -137,21 +178,48 @@ def Computing():
 
     # SHOW SINOGRAM
     sinogram = radon_t(img)
-    if anime.get() is 0:
-        sinogram_to_array = IMG.fromarray(np.array(sinogram))
-        sinogram_photo = IMGtk.PhotoImage(sinogram_to_array)
-        sinogram1.create_image(0, 0, image=sinogram_photo, anchor='nw')
-        root.update()
+
+    sinogram_to_array = IMG.fromarray(np.array(sinogram))
+    sinogram_photo = IMGtk.PhotoImage(sinogram_to_array)
+    sinogram1.create_image(0, 0, image=sinogram_photo, anchor='nw')
+    root.update()
     #else:
-    #    animation(sinogram)
+     #   animation(sinogram)
 
     # SHOW OUTPUT
-    reconstruction = i_radon_t(sinogram, img.shape[0])
+    reconstruction = i_radon_t(sinogram, img.shape[0],img)
     output_to_array = IMG.fromarray(np.array(reconstruction))
     output_photo = IMGtk.PhotoImage(output_to_array)
     output.create_image(0, 0, image=output_photo, anchor='nw')
+    dicomF(reconstruction, name_input.get(), description_input.get(), sex_input.get(),birth_date_input.get())
+    MSE_val.set("RMSE:" + str(process_MSE(img, reconstruction)))
+    print("RSME="+str(process_MSE(img,reconstruction)))
     root.update()
     root.wait_window()
+
+def dicomF(image, name, description, sex, birthDate):
+    filename = get_testdata_files("CT_small.dcm")[0]
+    ds = pydicom.dcmread(filename)
+
+    image2 = np.asarray(image, dtype=np.uint16)
+    ds.Rows = image2.shape[1]
+    ds.Columns = image2.shape[0]
+    ds.PixelData = image2.tostring()
+
+    ds.PatientName = name
+    ds.PatientSex = sex
+    ds.PatientBirthDate = birthDate
+    dt = datetime.datetime.now()
+    ds.StudyDate = dt.strftime('%Y%m%d')
+    timeStr = dt.strftime('%H%M%S.%d')
+    ds.StudyTime = timeStr
+    ds.InstitutionName = 'PP'
+    ds.StudyDescription = description
+
+    ds.save_as("Tomograf2_DICOM.dcm")
+
+def process_MSE(input_image, current_image):
+    return round(np.sqrt(((input_image - current_image) ** 2).mean()), 2)
 
 
 if __name__ == '__main__':
@@ -168,25 +236,33 @@ if __name__ == '__main__':
     sinogram1 = tk.Canvas(root, width=400, height=400)
     sinogram1.grid(row=0, column=1)
 
-    output = tk.Canvas(root, width=400, height=400)
+    output = tk.Canvas(root, width=1024, height=1024)
     output.grid(row=0, column=2)
 
-    inputs = tk.Canvas(root, width=400, height=100)
-    inputs.grid(row=1, column=0)
+    inputs = tk.Canvas(root, width=400, height=400)
+    inputs.grid(row=0, column=3)
 
-    # Patient first name
-    first_name = tk.Label(inputs, text='First name', fg="black")
-    first_name.grid(row=0, column=0)
+    # Patient name
+    name = tk.Label(inputs, text='Name', fg="black")
+    name.grid(row=0, column=0)
 
-    first_name_input = tk.Entry(inputs)
-    first_name_input.grid(row=0, column=1)
+    name_input = tk.Entry(inputs)
+    name_input.grid(row=0, column=1)
 
-    # Patient last name
-    last_name = tk.Label(inputs, text='Last name', fg="black")
-    last_name.grid(row=1, column=0)
+    # Patient birth date
+    birth_date = tk.Label(inputs, text='Sex', fg="black")
+    birth_date.grid(row=1, column=0)
 
-    last_name_input = tk.Entry(inputs)
-    last_name_input.grid(row=1, column=1)
+    birth_date_input = tk.Entry(inputs)
+    birth_date_input.grid(row=1, column=1)
+
+
+    # Patient sex
+    sex = tk.Label(inputs, text='Sex', fg="black")
+    sex.grid(row=1, column=0)
+
+    sex_input = tk.Entry(inputs)
+    sex_input.grid(row=1, column=1)
 
     # Description
     description = tk.Label(inputs, text='Description', fg="black")
@@ -227,14 +303,23 @@ if __name__ == '__main__':
     animation = tk.Checkbutton(inputs, text='Animation', variable=anime, onvalue=1, offvalue=0)
     animation.grid(row=7, column=0)
 
+    #
+    filtr= tk.IntVar()
+    filter = tk.Checkbutton(inputs, text='Filtr', variable=filtr, onvalue=1, offvalue=0)
+    filter.grid(row=8, column=0)
+
     # BUTTONS
      # quit
     quit = tk.Button(inputs, text='Exit', command=root.destroy)
-    quit.grid(row=8, column=0)
+    quit.grid(row=9, column=0)
      # start computing
     start = tk.Button(inputs, text='Start', command=Computing)
-    start.grid(row=8, column=1)
+    start.grid(row=9, column=1)
 
+    MSE_val = tk.DoubleVar()
+    MSE_val.set("RMSE:")
+    RMSE_label = tk.Label(inputs, textvariable=MSE_val)
+    RMSE_label.grid(row=10, column=0)
 
     root.mainloop()
     root.wait_window()
